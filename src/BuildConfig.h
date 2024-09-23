0851421021b6cffc46cc35309c5c1f7257db0c13
Unfinished632 <username1653e632@gmail.com>
2024-09-23 20:08:08 +0300

    Put the help text into a `HELP_TEXT` macro and made the program print the help text when ran with no arguments instead of error

--- src/BuildConfig.h
+++ src/BuildConfig.h
@@ -1,2 +1,11 @@
+//Defined by CMake
 #define onlang_VERSION_MAJOR 0
 #define onlang_VERSION_MINOR 1
+
+#define HELP_TEXT \
+"Commands:\n" << \
+"   --help => Prints all possible arguments and what they do.\n" << \
+"   --version => Prints the version of the program.\n" << \
+"   <onFilePath> => Interprets the file.\n" << \
+"   -lexed <onFilePath> => Prints the lexed version of the file.\n" << \
+"   -ast <onFilePath> => Prints the AST of the file.\n"
--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,12 @@ void RunCode(std::string code);
 
 int main(int argc, char* argv[]){
     if (argc <= 1){
-        std::cout << "error: No arguments\n";
+        std::cout << HELP_TEXT;
         return 0;
     }
 
     if (!strcmp(argv[1], "--help"))
-        std::cout << "Commands:\n" <<
-        "   --help => Prints all possible arguments and what they do.\n" <<
-        "   --version => Prints the version of the program.\n" <<
-        "   <onFilePath> => Interprets the file.\n" <<
-        "   -lexed <onFilePath> => Prints the lexed version of the file.\n" <<
-        "   -ast <onFilePath> => Prints the AST of the file.\n"; 
+        std::cout << HELP_TEXT;
     else if (!strcmp(argv[1], "--version"))
         std::cout << "On version " << onlang_VERSION_MAJOR << '.' << onlang_VERSION_MINOR << '\n';
     else if (!strcmp(argv[1], "-lexed"))
